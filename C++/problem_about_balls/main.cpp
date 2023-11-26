dc70a28357abdafba45704b474c48cc19bd08d73
Daniil Gold <golddaniiil13@gmail.com>
2023-11-26 22:15:06 +0300

    main.cpp

--- C++/problem_about_balls/main.cpp
+++ C++/problem_about_balls/main.cpp
@@ -11,55 +11,56 @@
 using namespace std;
 
 void print_permutation(int permutation[], int size) {
-	for (int i = 0; i < size; i++) {
-		cout << permutation[i] << " ";
-	}
-	cout << endl;
+    for (int i = 0; i < size; i++) {
+        cout << permutation[i] << " ";
+    }
+    cout << endl;
 }
 
-void generate_permutation(int permutation[], int m, int n, int i) {
-	if (i == n) {
-		print_permutation(permutation, n);
-		return;
-	}
+void generate_permutation(int permutation[], int m, int n, int i, int& count) {
+    if (i == n) {
+        print_permutation(permutation, n);
+        count++;
+        return;
+    }
 
-	if (i == m) {
-		print_permutation(permutation, n);
-	}
-	else {
-		for (int j = i; j < n; j++) {
-			swap(permutation[i], permutation[j]);
-			generate_permutation(permutation, m, n, i + 1);
-			swap(permutation[i], permutation[j]);
-		}
-	}
+    if (i == m) {
+        print_permutation(permutation, n);
+    }
+    else {
+        for (int j = i; j < n; j++) {
+            swap(permutation[i], permutation[j]);
+            generate_permutation(permutation, m, n, i + 1, count);
+            swap(permutation[i], permutation[j]);
+        }
+    }
 }
 
 int main() {
-	int n;
-	while (true) {
-		cout << "enter the number of balls: ";
-		if (!(cin >> n)) {
-			cin.clear();
-			cin.ignore();
-			cout << "error\n";
-			continue;
-		}
-		break;
-	}
+    int n;
+    while (true) {
+        cout << "enter the number of balls: ";
+        if (!(cin >> n)) {
+            cin.clear();
+            cin.ignore();
+            cout << "error\n";
+            continue;
+        }
+        break;
+    }
 
-	int* balls = new int[n];
-	for (int i = 0; i < n; i++) {
-		balls[i] = i + 1;
-	}
+    int* balls = new int[n];
+    for (int i = 0; i < n; i++) {
+        balls[i] = i + 1;
+    }
 
-	int count = 0;
+    int count = 0;
 
-	generate_permutation(balls, n, n, 1);
+    generate_permutation(balls, n, n, 0, count);
 
-	cout << "kolvo : " << count << endl;
+    cout << "\ntotal number of permutations: " << count << endl;
 
-	delete[] balls;
+    delete[] balls;
 
-	return 0;
+    return 0;
 }
