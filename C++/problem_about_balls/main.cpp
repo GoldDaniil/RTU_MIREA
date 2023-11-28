96749e4432f2b2a390f518af03a7fc3d934bf3a5
Daniil Gold <golddaniiil13@gmail.com>
2023-11-29 00:13:50 +0300

    main.cpp

--- C++/problem_about_balls/main.cpp
+++ C++/problem_about_balls/main.cpp
@@ -10,62 +10,46 @@ void print_permutation(int permutation[], int size) {
     cout << endl;
 }
 
-bool is_valid_permutation(int permutation[], int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        if (permutation[i] == 1 && permutation[i + 1] == 2) {
-            return true;
-        }
-    }
-    return false;
-}
-
 void generate_permutation(int permutation[], int n, int i, int& count) {
     if (i == n) {
-        if (is_valid_permutation(permutation, n)) {
-            print_permutation(permutation, n);
-            count++;
-        }
+        count++;
         return;
     }
 
     for (int j = i; j < n; j++) {
         swap(permutation[i], permutation[j]);
+
         generate_permutation(permutation, n, i + 1, count);
+
         swap(permutation[i], permutation[j]);
     }
 }
 
 int main() {
+    int n;
     while (true) {
-        int n;
-        while (true) {
-            cout << "enter the number of balls: ";
-            if (!(cin >> n)) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                cout << "error\n";
-                continue;
-            }
-            break;
-        }
-
-        if (n <= 0) {
-            cout << "Number of balls should be positive.\n";
+        cout << "Enter the number of balls: ";
+        if (!(cin >> n)) {
+            cin.clear();
+            cin.ignore();
+            cout << "Error\n";
             continue;
         }
-
-        int* balls = new int[n];
-        for (int i = 0; i < n; i++) {
-            balls[i] = i + 1;
-        }
-
-        int count = 0;
-
-        generate_permutation(balls, n, 0, count);
-
-        cout << "\ntotal number of valid permutations: " << count << endl;
-        delete[] balls;
+        break;
     }
 
+    int* balls = new int[n];
+    for (int i = 0; i < n; i++) {
+        balls[i] = i + 1;
+    }
+
+    int count = 0;
+
+    generate_permutation(balls, n, 0, count);
+
+    cout << "\nTotal number of permutations with at least one match: " << count << endl;
+
+    delete[] balls;
+    
     return 0;
 }
