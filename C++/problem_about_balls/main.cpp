6338da1b7feea6ed041f2248d31bc72be3b7278c
Daniil Gold <golddaniiil13@gmail.com>
2023-11-28 23:46:24 +0300

    main.cpp

--- C++/problem_about_balls/main.cpp
+++ C++/problem_about_balls/main.cpp
@@ -1,12 +1,5 @@
-// The plot of the problem is about balls. One ball is taken out of the urn with 10 numbered balls.
-// Calculate the total number of situations when the number of at least one removed ball coincides
-// with the sequence number of the "taking out" action, for example, when ball No. 3 is taken out
-// by the 3rd in order.
-// Сюжет задачи про шарики. Из урны с 10 пронумерованными шариками вынимают по одному шарику. 
-// Подсчитать общее количество ситуаций, когда номер хотя бы одного вынутого шарика совпадает 
-// с порядковым номером действия "вынимания", например, когда шарик № 3 будет вынут 3-им по порядку.
-
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,50 +10,62 @@ void print_permutation(int permutation[], int size) {
     cout << endl;
 }
 
-void generate_permutation(int permutation[], int m, int n, int i, int& count) {
+bool is_valid_permutation(int permutation[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        if (permutation[i] == 1 && permutation[i + 1] == 2) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void generate_permutation(int permutation[], int n, int i, int& count) {
     if (i == n) {
-        print_permutation(permutation, n);
-        count++;
+        if (is_valid_permutation(permutation, n)) {
+            print_permutation(permutation, n);
+            count++;
+        }
         return;
     }
 
-    if (i == m) {
-        print_permutation(permutation, n);
-    }
-    else {
-        for (int j = i; j < n; j++) {
-            swap(permutation[i], permutation[j]);
-            generate_permutation(permutation, m, n, i + 1, count);
-            swap(permutation[i], permutation[j]);
-        }
+    for (int j = i; j < n; j++) {
+        swap(permutation[i], permutation[j]);
+        generate_permutation(permutation, n, i + 1, count);
+        swap(permutation[i], permutation[j]);
     }
 }
 
 int main() {
-    int n;
     while (true) {
-        cout << "enter the number of balls: ";
-        if (!(cin >> n)) {
-            cin.clear();
-            cin.ignore();
-            cout << "error\n";
+        int n;
+        while (true) {
+            cout << "enter the number of balls: ";
+            if (!(cin >> n)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "error\n";
+                continue;
+            }
+            break;
+        }
+
+        if (n <= 0) {
+            cout << "Number of balls should be positive.\n";
             continue;
         }
-        break;
+
+        int* balls = new int[n];
+        for (int i = 0; i < n; i++) {
+            balls[i] = i + 1;
+        }
+
+        int count = 0;
+
+        generate_permutation(balls, n, 0, count);
+
+        cout << "\ntotal number of valid permutations: " << count << endl;
+        delete[] balls;
     }
-
-    int* balls = new int[n];
-    for (int i = 0; i < n; i++) {
-        balls[i] = i + 1;
-    }
-
-    int count = 0;
-
-    generate_permutation(balls, n, n, 0, count);
-
-    cout << "\ntotal number of permutations: " << count << endl;
-
-    delete[] balls;
 
     return 0;
 }
