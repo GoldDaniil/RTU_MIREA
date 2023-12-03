9f71d11664f5175c860737b0d0bb54051f6b8e55
Daniil Gold <golddaniiil13@gmail.com>
2023-12-03 15:25:01 +0300

    main_task_100.cpp

--- C++/automatic_tasks/main_task_100.cpp
+++ C++/automatic_tasks/main_task_100.cpp
@@ -1,9 +1,9 @@
 // The code uses the snake_case naming style for functions and variables.
 // CamelCase is used for classes and namespace.
 
-#include<iostream>
-#include<algorithm>
-#include<cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
@@ -25,22 +25,20 @@ void fill_matrix_randomly(char matrix[MAX_SIZE][MAX_SIZE], char symbols[], int o
 }
 
 void adapt_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order, char selected_symbol) {
-    int neighbors[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-    char new_matrix[MAX_SIZE][MAX_SIZE] = { 0 }; 
+    char new_matrix[MAX_SIZE][MAX_SIZE] = { 0 };
 
     for (int i = 0; i < order; ++i) {
         for (int j = 0; j < order; ++j) {
             int live_neighbors = 0;
 
-            // проверяем соседей
-            for (int k = 0; k < 12; ++k) {
-                int ni = (i + (k % 3) - 1 + order) % order; // соседний индекс по строкам 
-                int nj = (j + (k / 3) - 1 + order) % order; // соседний индекс по столбцам 
-
-                live_neighbors += (matrix[ni][nj] == selected_symbol) ? 1 : 0;
+            for (int ni = i - 1; ni <= i + 1; ++ni) {
+                for (int nj = j - 1; nj <= j + 1; ++nj) {
+                    if (ni >= 0 && ni < order && nj >= 0 && nj < order && !(ni == i && nj == j)) {
+                        live_neighbors += (matrix[ni][nj] == selected_symbol) ? 1 : 0;
+                    }
+                }
             }
 
-            // вносим правила игры про жизнь клетки
             if (matrix[i][j] == selected_symbol) {
                 new_matrix[i][j] = (live_neighbors == 2 || live_neighbors == 3) ? selected_symbol : '0';
             }
@@ -50,7 +48,6 @@ void adapt_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order, char selected_symb
         }
     }
 
-    // копируем
     for (int i = 0; i < order; ++i) {
         for (int j = 0; j < order; ++j) {
             matrix[i][j] = new_matrix[i][j];
@@ -104,7 +101,7 @@ int main() {
     }
     catch (const exception& err) {
         cerr << "error: " << err.what() << endl;
-        return 1; // Exit with an error code
+        return 1; 
     }
 
     char symbols[MAX_SIZE];
@@ -116,13 +113,12 @@ int main() {
     }
     cout << endl;
 
-    char matrix[MAX_SIZE][MAX_SIZE] = { 0 }; 
+    char matrix[MAX_SIZE][MAX_SIZE] = { 0 };
     fill_matrix_randomly(matrix, symbols, order, num_symbols);
 
     cout << "\ninitial two-dimensional array (matrix): \n";
     print_matrix(matrix, order);
 
-    // рандомно выбираем уникальный символ для генерации
     char selected_symbol = symbols[rand() % num_symbols];
 
     cout << "\nselected Symbol for Adaptation: " << selected_symbol << endl;
@@ -136,6 +132,149 @@ int main() {
 
     return 0;
 }
+
+
+
+//
+//
+//
+//
+//
+//#include<iostream>
+//#include<algorithm>
+//#include<cstdlib>
+//
+//using namespace std;
+//
+//const int MAX_SIZE = 100;
+//
+//void generate_unique_symbols(char symbols[], int num_symbols) {
+//    for (int i = 0; i < num_symbols; ++i) {
+//        symbols[i] = 'a' + i;
+//    }
+//}
+//
+//void fill_matrix_randomly(char matrix[MAX_SIZE][MAX_SIZE], char symbols[], int order, int num_symbols) {
+//    random_shuffle(symbols, symbols + num_symbols);
+//    for (int i = 0; i < order; ++i) {
+//        for (int j = 0; j < order; ++j) {
+//            matrix[i][j] = symbols[j];
+//        }
+//    }
+//}
+//
+//void adapt_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order, char selected_symbol) {
+//    int neighbors[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+//    char new_matrix[MAX_SIZE][MAX_SIZE] = { 0 };
+//
+//    for (int i = 0; i < order; ++i) {
+//        for (int j = 0; j < order; ++j) {
+//            int live_neighbors = 0;
+//
+//            // проверяем соседей
+//            for (int k = 0; k < 12; ++k) {
+//                int ni = (i + (k % 3) - 1 + order) % order; // соседний индекс по строкам 
+//                int nj = (j + (k / 3) - 1 + order) % order; // соседний индекс по столбцам 
+//
+//                live_neighbors += (matrix[ni][nj] == selected_symbol) ? 1 : 0;
+//            }
+//
+//            // вносим правила игры про жизнь клетки
+//            if (matrix[i][j] == selected_symbol) {
+//                new_matrix[i][j] = (live_neighbors == 2 || live_neighbors == 3) ? selected_symbol : '0';
+//            }
+//            else {
+//                new_matrix[i][j] = (live_neighbors == 3) ? selected_symbol : '0';
+//            }
+//        }
+//    }
+//
+//    // копируем
+//    for (int i = 0; i < order; ++i) {
+//        for (int j = 0; j < order; ++j) {
+//            matrix[i][j] = new_matrix[i][j];
+//        }
+//    }
+//}
+//
+//void print_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order) {
+//    for (int i = 0; i < order; ++i) {
+//        for (int j = 0; j < order; ++j) {
+//            cout << matrix[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//}
+//
+//int main() {
+//    int order, num_symbols, max_generations;
+//
+//    try {
+//        while (true) {
+//            cout << "\nenter the order of the matrix (2, 3, or 4): ";
+//            if (!(cin >> order) || (order < 2 || order > 4)) {
+//                cin.clear();
+//                cin.ignore();
+//                cout << "error!\n";
+//                continue;
+//            }
+//            break;
+//        }
+//        while (true) {
+//            cout << "\nenter the number of unique characters: ";
+//            if (!(cin >> num_symbols) || num_symbols > 26) {
+//                cin.clear();
+//                cin.ignore();
+//                cout << "error!\n";
+//                continue;
+//            }
+//            break;
+//        }
+//        while (true) {
+//            cout << "\nenter the maximum number of generations: ";
+//            if (!(cin >> max_generations)) {
+//                cin.clear();
+//                cin.ignore();
+//                cout << "error!\n";
+//                continue;
+//            }
+//            break;
+//        }
+//    }
+//    catch (const exception& err) {
+//        cerr << "error: " << err.what() << endl;
+//        return 1; // Exit with an error code
+//    }
+//
+//    char symbols[MAX_SIZE];
+//    generate_unique_symbols(symbols, num_symbols);
+//
+//    cout << "\nunique characters: \n";
+//    for (int i = 0; i < num_symbols; ++i) {
+//        cout << symbols[i] << " ";
+//    }
+//    cout << endl;
+//
+//    char matrix[MAX_SIZE][MAX_SIZE] = { 0 };
+//    fill_matrix_randomly(matrix, symbols, order, num_symbols);
+//
+//    cout << "\ninitial two-dimensional array (matrix): \n";
+//    print_matrix(matrix, order);
+//
+//    // рандомно выбираем уникальный символ для генерации
+//    char selected_symbol = symbols[rand() % num_symbols];
+//
+//    cout << "\nselected Symbol for Adaptation: " << selected_symbol << endl;
+//
+//    for (int generation = 1; generation <= max_generations; ++generation) {
+//        adapt_matrix(matrix, order, selected_symbol);
+//
+//        cout << "\ngeneration " << generation << ":\n";
+//        print_matrix(matrix, order);
+//    }
+//
+//    return 0;
+//}
 
 
 
