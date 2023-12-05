ae3cf7c942f482ca044b0dc1babe4130a62ad540
Daniil Gold <golddaniiil13@gmail.com>
2023-12-05 16:37:54 +0300

    Create test_two_main.cpp

---
+++ C++/automatic_tasks/variants_151/test_two_main.cpp
@@ -0,0 +1,54 @@
+
+
+
+#include<vector>
+#include<iostream>
+
+
+using namespace std;
+
+
+
+void populations(const int initial_predators, const int initial_herbivores, const int predators_AGE_MIN, const int herbivores_AGE_MAX, const int predators_reproductive_AGE_MIN, const int herbivores_reproductive_AGE_MIN, const int predators_birth_rate, const int herbivores_birth_rate, const int probability_natural_disasters, const int herbivores_numberfood_predator, const int herbivore_amountfood_grass, const int percentage_restoration_amountgrass, const int herbivores_restoredyear_peryear, const int herbivores_amount_peryear) {
+	//добавить проверку на репродуктивный возраст(взять формулы из инета - биология).
+}
+
+class Predators {
+public:
+	void live(const int MAX_AGE) {
+
+	}
+	private:
+
+		
+		
+};
+
+
+class Herbivores {
+public:
+
+
+private:
+};
+
+
+namespace EmulationProcess {
+	class DisplayCout {
+
+
+		void cout_probabilities() {
+
+		}
+	};
+
+
+}
+
+
+int main() {
+	cout << " \n";
+	
+
+
+}
