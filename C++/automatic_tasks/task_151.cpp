e5ab19b241831e731d21e8fe3a1870ad79ac7459
Daniil Gold <golddaniiil13@gmail.com>
2023-12-05 16:52:34 +0300

    task_151.cpp

--- C++/automatic_tasks/task_151.cpp
+++ C++/automatic_tasks/task_151.cpp
@@ -1,55 +1,113 @@
+#include <iostream>
+#include <cstdlib>
 
+const int MAX_ANIMALS = 1000; // макс колво животных
 
-
-#include<vector>
-#include<iostream>
-
-
-using namespace std;
-
-
-
-void populations(const int initial_predators, const int initial_herbivores, const int predators_AGE_MIN, const int herbivores_AGE_MAX, const int predators_reproductive_AGE_MIN, const int herbivores_reproductive_AGE_MIN, const int predators_birth_rate, const int herbivores_birth_rate, const int probability_natural_disasters, const int herbivores_numberfood_predator, const int herbivore_amountfood_grass, const int percentage_restoration_amountgrass, const int herbivores_restoredyear_peryear, const int herbivores_amount_peryear) {
-	//добавить проверку на репродуктивный возраст(взять формулы из инета - биология).
-}
-
-class Predators {
-public:
-	void live(const int MAX_AGE) {
-
-	}
-	private:
-
-		
-		
+struct Animal {
+    int age;
+    int reproductive_age_min;
+    int reproductive_age_max;
+    int max_age;
+    double reproduction_rate;
 };
 
-
-class Herbivores {
-public:
-
-
-private:
+struct Environment {
+    int screen_width;
+    int screen_height;
+    int initial_grass;
+    double grass_regrowth_rate;
+    double natural_disaster_probability;
+    int herbivore_food_needed;
+    int grass_food_needed;
+    double grass_recovery_rate;
+    double herbivore_food_recovery;
 };
 
-
-namespace EmulationProcess {
-	class DisplayCout {
-
-
-		void cout_probabilities() {
-
-		}
-	};
-
-
+void initialize_herbivore_population(Animal herbivores[], int initial_population, int max_age, int reproductive_age_min, int reproductive_age_max, double reproduction_rate) {
+    for (int i = 0; i < initial_population; ++i) {
+        herbivores[i].age = rand() % (max_age + 1);
+        herbivores[i].reproductive_age_min = reproductive_age_min;
+        herbivores[i].reproductive_age_max = reproductive_age_max;
+        herbivores[i].max_age = max_age;
+        herbivores[i].reproduction_rate = reproduction_rate;
+    }
 }
 
+void initialize_predator_population(Animal predators[], int initial_population, int max_age, int reproductive_age_min, int reproductive_age_max, double reproduction_rate) {
+    for (int i = 0; i < initial_population; ++i) {
+        predators[i].age = rand() % (max_age + 1);
+        predators[i].reproductive_age_min = reproductive_age_min;
+        predators[i].reproductive_age_max = reproductive_age_max;
+        predators[i].max_age = max_age;
+        predators[i].reproduction_rate = reproduction_rate;
+    }
+}
 
 int main() {
-	cout << " \n";
-	
+    using namespace std;
 
+    // инициализация параметров окружающей среды
+    Environment environment;
+    cout << "Enter environmental parameters:\n";
+    cout << "screen width: ";
+    cin >> environment.screen_width;
+    cout << "screen height: ";
+    cin >> environment.screen_height;
+    cout << "initial grass quantity: ";
+    cin >> environment.initial_grass;
+    cout << "grass regrowth rate: ";
+    cin >> environment.grass_regrowth_rate;
+    cout << "probability of natural disasters: ";
+    cin >> environment.natural_disaster_probability;
+    cout << "number of herbivores needed for predator's food: ";
+    cin >> environment.herbivore_food_needed;
+    cout << "amount of grass needed for herbivore's food: ";
+    cin >> environment.grass_food_needed;
+    cout << "percentage of grass recovery: ";
+    cin >> environment.grass_recovery_rate;
+    cout << "amount of herbivore food recovered per year: ";
+    cin >> environment.herbivore_food_recovery;
 
+    // ввод данных о популяции травоядных и хищников
+    int initial_herbivore_population, initial_predator_population;
+    int max_herbivore_age, max_predator_age;
+    int reproductive_age_min_herbivore, reproductive_age_max_herbivore, reproductive_age_min_predator, reproductive_age_max_predator;
+    double reproduction_rate_herbivore, reproduction_rate_predator;
+
+    //цвет
+    cout << "Enter herbivore information:\n";
+    //цвет
+    cout << "initial quantity: ";
+    cin >> initial_herbivore_population;
+    cout << "maximum age: ";
+    cin >> max_herbivore_age;
+    cout << "minimum age for reproduction: ";
+    cin >> reproductive_age_min_herbivore;
+    cout << "maximum breeding age: ";
+    cin >> reproductive_age_max_herbivore;
+    cout << "reproduction level: ";
+    cin >> reproduction_rate_herbivore;
+
+    //цвет
+    cout << "Enter predator data:\n";
+    //цвет
+    cout << "initial quantity: ";
+    cin >> initial_predator_population;
+    cout << "maximum age: ";
+    cin >> max_predator_age;
+    cout << "minimum age for reproduction: ";
+    cin >> reproductive_age_min_predator;
+    cout << "maximum breeding age: ";
+    cin >> reproductive_age_max_predator;
+    cout << "reproduction level: ";
+    cin >> reproduction_rate_predator;
+
+    // инициализация популяции травоядных и хищников
+    Animal herbivores[MAX_ANIMALS];
+    initialize_herbivore_population(herbivores, initial_herbivore_population, max_herbivore_age, reproductive_age_min_herbivore, reproductive_age_max_herbivore, reproduction_rate_herbivore);
+
+    Animal predators[MAX_ANIMALS];
+    initialize_predator_population(predators, initial_predator_population, max_predator_age, reproductive_age_min_predator, reproductive_age_max_predator, reproduction_rate_predator);
+
+    return 0;
 }
-
