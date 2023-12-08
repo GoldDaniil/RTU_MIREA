7a0c05e62ee409e2917194318b04a727a17f28db
Daniil Gold <golddaniiil13@gmail.com>
2023-12-08 17:50:40 +0300

    test_one_main.cpp

--- C++/automatic_tasks/variants_100/test_one_main.cpp
+++ C++/automatic_tasks/variants_100/test_one_main.cpp
@@ -1,133 +1,288 @@
 // The code uses the snake_case naming style for functions and variables.
 // CamelCase is used for classes and namespace.
 
-#include <iostream>
-#include <algorithm>
-#include <cstdlib>
+#include <iostream>            
+#include <vector>              
+#include <cstdlib>             // 17-50 версия с равными колвами уникальных символов
+#include <ctime>               
+#include <algorithm>           
+#include <Windows.h>           
 
 using namespace std;
 
-const int MAX_SIZE = 100;
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void generate_unique_symbols(char symbols[], int num_symbols) {
-    for (int i = 0; i < num_symbols; ++i) {
-        symbols[i] = 'a' + i;
+enum CellState { DEAD = 0, ALIVE = 1, MAX_AGE = 12 };    // перечисление возможных состояний ячейки
+
+struct Cell {
+    int state;    // состояние ячейки (мертвая или живая)
+    int age;      // возраст ячейки
+
+    Cell() : state(DEAD), age(0) {}    // конструктор для инициализации ячейки
+};
+
+class GameOfLife {
+public:
+    GameOfLife(int size, int max_years, const vector<char>& symbols)
+        : size(size), max_years(max_years), symbols(symbols) {
+        srand(time(0));    // генератор случайных чисел текущим временем
+        grid.resize(size, vector<Cell>(size, Cell()));    // инициализация игровой сетки заданного размера
+        initialize_grid_manually();    // инициализация игровой сетки вручную
     }
-}
 
-void fill_matrix_randomly(char matrix[MAX_SIZE][MAX_SIZE], char symbols[], int order, int num_symbols) {
-    random_shuffle(symbols, symbols + num_symbols);
-    for (int i = 0; i < order; ++i) {
-        for (int j = 0; j < order; ++j) {
-            matrix[i][j] = symbols[j];
+    void initialize_grid_manually() {
+        cout << "\nunique symbols:\n";
+
+        // вывод уникальных символов для удобства
+        for (char ch : symbols) {
+            cout << ch << " ";
         }
+        cout << endl;
+
+        // случайным образом выбираем один из символов и обновляем основной двумерный массив
+        int random_symbol_index = rand() % symbols.size();
+        selected_symbol = symbols[random_symbol_index];
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                grid[i][j].state = (symbols[j] == selected_symbol) ? ALIVE : DEAD;
+            }
+        }
+
+        cout << "\nselected symbol for adaptation: " << selected_symbol << endl << endl;
     }
-}
 
-void adapt_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order, char selected_symbol) {
-    char new_matrix[MAX_SIZE][MAX_SIZE] = { 0 };
+    void print_grid() const {
+        cout << endl;
 
-    for (int i = 0; i < order; ++i) {
-        for (int j = 0; j < order; ++j) {
-            int live_neighbors = 0;
-
-            for (int ni = i - 1; ni <= i + 1; ++ni) {
-                for (int nj = j - 1; nj <= j + 1; ++nj) {
-                    if (ni >= 0 && ni < order && nj >= 0 && nj < order && !(ni == i && nj == j)) {
-                        live_neighbors += (matrix[ni][nj] == selected_symbol) ? 1 : 0;
-                    }
+        // вывод текущего состояния игровой сетки в консоль
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (grid[i][j].state == ALIVE) {
+                    cout << "\033[42m    \033[0m";    // зеленый цвет для живых клеток
+                }
+                else {
+                    cout << "\033[47;90m    \033[0m"; // серый цвет для мертвых клеток
                 }
             }
-
-            if (matrix[i][j] == selected_symbol) {
-                new_matrix[i][j] = (live_neighbors == 2 || live_neighbors == 3) ? selected_symbol : '0';
-            }
-            else {
-                new_matrix[i][j] = (live_neighbors == 3) ? selected_symbol : '0';
-            }
+            cout << endl;
         }
-    }
+        cout << endl;
 
-    for (int i = 0; i < order; ++i) {
-        for (int j = 0; j < order; ++j) {
-            matrix[i][j] = new_matrix[i][j];
-        }
-    }
-}
+        cout << endl << "age:" << endl;
 
-void print_matrix(char matrix[MAX_SIZE][MAX_SIZE], int order) {
-    for (int i = 0; i < order; ++i) {
-        for (int j = 0; j < order; ++j) {
-            cout << matrix[i][j] << " ";
+        // вывод возраста живых клеток
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (grid[i][j].state == ALIVE) {
+                    if (grid[i][j].age > 0) {
+                        cout << " " << grid[i][j].age << " ";
+                    }
+                    else {
+                        cout << " 1 ";
+                    }
+                }
+                else {
+                    cout << " 0 ";
+                }
+            }
+            cout << endl;
         }
         cout << endl;
     }
+
+    void simulate_life() {
+        for (int year = 1; year <= max_years; ++year) {
+            cout << "generation " << year << ":" << endl;
+            print_grid();
+            evolve();    // эволюция игры на один шаг
+
+            if (is_game_over()) {
+                cout << "\nGame over!\nAll microbes are dead!\n" << endl;
+                break;
+            }
+        }
+    }
+
+private:
+    void evolve() {
+        vector<vector<Cell>> new_grid(size, vector<Cell>(size, Cell()));
+
+        // Процесс эволюции игры
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                int neighbors = count_neighbors(i, j);
+
+                // Если клетка живая
+                if (grid[i][j].state == 1) {
+                    if (neighbors == 2 || neighbors == 3) {
+                        // Клетка остается живой
+                        new_grid[i][j].state = 1;
+                        // Увеличиваем её возраст на 1
+                        new_grid[i][j].age = grid[i][j].age + 1;
+
+                        // Если клетке 12 лет и больше - она умирает
+                        if (new_grid[i][j].age >= 12) {
+                            new_grid[i][j].state = 0;
+                            new_grid[i][j].age = 0;
+                        }
+                    }
+                }
+                // Если клетка мертвая
+                else {
+                    if (neighbors == 3) {
+                        new_grid[i][j].state = 1;
+                        new_grid[i][j].age = 1;
+                    }
+                }
+            }
+        }
+
+        // Обновляем текущую сетку
+        grid = new_grid;
+    }
+
+    //void evolve() {
+    //    vector<vector<Cell>> new_grid(size, vector<Cell>(size, Cell()));
+
+    //    // процесс эволюции игры
+    //    for (int i = 0; i < size; ++i) {
+    //        for (int j = 0; j < size; ++j) {
+    //            int neighbors = count_neighbors(grid, i, j);
+
+
+
+    //            // если grid[i][j].state живая {
+    //            //    если у клетки 2 или 3 соседа, она остается живой
+    //            // 
+    //            //    увеличиваем её возраст на 1
+    //            // 
+    //            //    если клетке 12 лет и больше - то она умирает - становится 0 
+    //            //
+    //            // } если grid[i][j].state мертвая {
+    //            //    если 3 микроба-соседа рядом то микроб 0 - оживает и превращается в 1
+    //            //           увеличиваем ее возраст на 1  - в противном случае клетка умирает
+    //            // }
+    //        }
+    //    }
+    //}
+
+    int count_neighbors(int x, int y) const {
+        //int count_neighbors(const vector<vector<Cell>>& board, int x, int y) const {
+        int count = 0;
+
+        // подсчет количества соседей для заданной клетки
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                int newX = x + i;
+                int newY = y + j;
+                //вычисляем новые координаты для проверки соседних клеток относительно текущей
+
+                // пропустить проверку для текущей клетки (i=0, j=0) - так как не учитываем текущую клетку
+                //  в подсчете живых соседей
+                if ((i == 0) && (j == 0)) {
+                    continue;
+                }
+
+                // проверка, находится ли сосед в пределах границ игровой сетки
+                if ((newX >= 0) && (newX < size) && (newY >= 0) && (newY < size)) {
+                    // если сосед жив - увеличиваем счетчик
+                    count += grid[newX][newY].state == ALIVE ? 1 : 0;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    bool is_game_over() const {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (grid[i][j].state == ALIVE) {
+                    return false; // если найдена хотя бы одна живая клетка - игра не завершена
+                }
+            }
+        }
+        return true; // если все клетки мертвы - то игра завершена
+    }
+
+    int size;                   // размер игровой сетки
+    int max_years;              // максимальное количество поколений
+    vector<vector<Cell>> grid;  // игровая сетка
+    vector<char> symbols;       // уникальные символы для инициализации
+    char selected_symbol;       // выбранный символ для адаптации
+};
+
+vector<char> generate_alphabet_symbols(int count) {
+    vector<char> symbols;
+    for (char ch = 'a'; ch < 'a' + count; ++ch) { //цикл - в котором переменная ch принимает 
+        // значения от 'a' до 'a' + count - 1.
+        symbols.push_back(ch); // добавление текущего символа в вектор
+    }
+    return symbols; // возврат вектора уникальных символов
 }
 
 int main() {
-    int order, num_symbols, max_generations;
+    const WORD textColor = 0xE;
+    const WORD bgColor = 0x70;
+    SetConsoleTextAttribute(console, textColor | (bgColor << 4));
+    cout << "Welcome to the game of 'LIFE'!\n\n";
+    SetConsoleTextAttribute(console, textColor);
+    cout << "Once upon a time there were microbes. They lived happily ever after,\nbut there was one problem: after life there was always death comes.\nWe have the opportunity to look at the development of microbes with our own eyes!\n\n";
+    SetConsoleTextAttribute(console, 15);
 
-    try {
+    int size, max_years;
+
+    while (true) {
+        // ввод размера матрицы
+        cout << "Enter matrix size: ";
         while (true) {
-            cout << "\nenter the order of the matrix (2, 3, or 4): ";
-            if (!(cin >> order) || (order < 2 || order > 4)) {
+            if (!(cin >> size)) {
                 cin.clear();
                 cin.ignore();
-                cout << "error!\n";
+                cout << "\nerror!\n\n";
                 continue;
             }
             break;
         }
+
+        int unique_symbol_count;
+
+        // ввод количества уникальных символов
+        cout << "Enter the number of unique symbols: ";
         while (true) {
-            cout << "\nenter the number of unique characters: ";
-            if (!(cin >> num_symbols) || num_symbols > 26) {
+            if (!(cin >> unique_symbol_count)) {
                 cin.clear();
                 cin.ignore();
-                cout << "error!\n";
+                cout << "\nerror!\n\n";
                 continue;
             }
             break;
         }
+
+        vector<char> symbols = generate_alphabet_symbols(unique_symbol_count);
+        // вызов функции generate_alphabet_symbols с аргументом unique_symbol_count - которая создает 
+        // вектор символов алфавита от 'a' до 'a' + unique_symbol_count - 1
+        // vector<char> symbols = ...: создает вектор symbols типа char и инициализирует его значениями -
+        // возвращенными из функции generate_alphabet_symbols
+
+        // ввод максимального числа поколений
+        cout << "Enter the maximum number of generations: ";
         while (true) {
-            cout << "\nenter the maximum number of generations: ";
-            if (!(cin >> max_generations)) {
+            if (!(cin >> max_years)) {
                 cin.clear();
                 cin.ignore();
-                cout << "error!\n";
+                cout << "\nerror!\n\n";
                 continue;
             }
             break;
         }
-    }
-    catch (const exception& err) {
-        cerr << "error: " << err.what() << endl;
-        return 1; 
-    }
 
-    char symbols[MAX_SIZE];
-    generate_unique_symbols(symbols, num_symbols);
+        GameOfLife game(size, max_years, symbols);
+        game.simulate_life();
 
-    cout << "\nunique characters: \n";
-    for (int i = 0; i < num_symbols; ++i) {
-        cout << symbols[i] << " ";
-    }
-    cout << endl;
-
-    char matrix[MAX_SIZE][MAX_SIZE] = { 0 };
-    fill_matrix_randomly(matrix, symbols, order, num_symbols);
-
-    cout << "\ninitial two-dimensional array (matrix): \n";
-    print_matrix(matrix, order);
-
-    char selected_symbol = symbols[rand() % num_symbols];
-
-    cout << "\nselected Symbol for Adaptation: " << selected_symbol << endl;
-
-    for (int generation = 1; generation <= max_generations; ++generation) {
-        adapt_matrix(matrix, order, selected_symbol);
-
-        cout << "\ngeneration " << generation << ":\n";
-        print_matrix(matrix, order);
+        // создается объект игры и затем запускают симуляцию "Жизнь микроба(бактерий)" - 
+        // выводя информацию о состоянии игры на каждом поколении
     }
 
     return 0;
