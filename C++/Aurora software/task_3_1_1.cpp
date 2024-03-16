493aaebcd7aa8202b43bc796b0ed07406dad1f2e
Daniil Gold <golddaniiil13@gmail.com>
2024-03-16 20:47:50 +0300

    task_3_1_1.cpp

--- C++/Aurora software/task_3_1_1.cpp
+++ C++/Aurora software/task_3_1_1.cpp
@@ -11,49 +11,41 @@ using namespace std;
 class ArrayObject {
 private:
     vector<int> array;
-    
+
 public:
     ArrayObject(int size) {
         array.resize(size, size);
     }
-    
+
     void set_second_object(const ArrayObject& second_object) {
         array = second_object.array;
     }
-    
+
     void print_array() const {
-        for(int num : array) {
-            cout << num << " ";
+        for(int i = 0; i < array.size(); ++i) {
+            cout << array[i];
+            if (i != array.size() - 1) {
+                cout << " ";
+            }
         }
-        cout << endl; //проверить на
-    }     // возможность утечки памяти
+        cout << endl;
+    }
 };
 
-int main()
-{
-    int input;
-    cin >> input;
-    
-    if(input <= 4) {
-        cout << input << "?";
+int main() {
+    int input1, input2;
+    cin >> input1 >> input2;
+
+    if(input1 <= 4 || input2 <= 4) {
+        cout << (input1 <= 4 ? input1 : input2) << "error";
         return 0;
     }
-    
-    ArrayObject first_object(input);
-    
-    cin >> input;
-    
-    if(input <= 4) {
-        cout << input << "?";
-        return 0;
-    }
-    
-    ArrayObject second_object(input);
-    
-    first_object.set_second_object(second_object);
-    
+
+    ArrayObject first_object(input1);
+    ArrayObject second_object(input2);
+
     first_object.print_array();
     second_object.print_array();
-    
+
     return 0;
 }
