146e1b6fbd06fd8c33926a5f4de3585b4ae1cb57
Daniil Gold <golddaniiil13@gmail.com>
2024-03-23 00:02:26 +0300

    Update main.py

--- Python/Discipline Artificial Intelligence/workbook_one/main.py
+++ Python/Discipline Artificial Intelligence/workbook_one/main.py
@@ -1,3 +1,4 @@
+#готово
 import math
 import math as m
 import numpy as np
@@ -198,7 +199,19 @@ def task_4_3_3():
 
 
 def task_4_3_4():
-    print("  ")
+    import yfinance as yf
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    a = yf.download('AAPL', '2021-01-01', '2021-12-31')
+    b = yf.download('MSFT', '2021-01-01', '2021-12-31')
+    d = yf.download('GOOG', '2021-01-01', '2021-12-31')
+
+    plt.grid(True)
+    plt.plot(a, color='g')
+    plt.plot(b, color='b')
+    plt.plot(d, color='r')
+    plt.show()
 
 
 def task_4_3_5():
