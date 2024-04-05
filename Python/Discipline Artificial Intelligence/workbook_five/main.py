275f4f63d666b03b5ed3f0892ef55d7675d5cdd9
Daniil Gold <golddaniiil13@gmail.com>
2024-04-05 22:32:38 +0300

    Update main.py

--- Python/Discipline Artificial Intelligence/workbook_five/main.py
+++ Python/Discipline Artificial Intelligence/workbook_five/main.py
@@ -75,10 +75,8 @@ def task_1_1():
     print("Arctangent: ", Trigonometry.arctangent(1))
 
 def task_1_2():
-    # Определение структуры дерева в виде списка из списков
     tree = ['A', ['B', ['D', [], []], ['E', [], []]], ['C', ['F', [], []], []]]
 
-    # Функция для создания графа из списка списков
     def create_tree_graph(tree, graph=None):
         if graph is None:
             graph = Digraph(format='png')
@@ -97,10 +95,8 @@ def task_1_2():
                 create_tree_graph(tree[2], graph)
         return graph
 
-    # Создание графа дерева
     tree_graph = create_tree_graph(tree)
 
-    # Вывод графа на экран
     tree_graph.render('tree')
     tree_graph.view()
 
@@ -137,10 +133,8 @@ def task_1_3():
 
     # Пример использования класса Tree
     if __name__ == "__main__":
-        # Создаем корень дерева
         root = Tree(10)
 
-        # Вставляем новые элементы
         root.insert(5)
         root.insert(15)
         root.insert(3)
@@ -148,7 +142,6 @@ def task_1_3():
         root.insert(12)
         root.insert(20)
 
-        # Выводим дерево
         plt.figure(figsize=(8, 8))
         root.print_tree()
         plt.xlim(-10, 10)
@@ -161,11 +154,9 @@ def task_1_3_1():
     X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
     target = [0, 0, 0, 1, 1, 1]
 
-    # Создание и обучение модели дерева принятия решений
     clf = DecisionTreeClassifier()
     clf.fit(X, target)
 
-    # Визуализация решающей поверхности
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
@@ -184,27 +175,20 @@ def task_1_3_1():
 def task_1_4_1():
     ssl._create_default_https_context = ssl._create_unverified_context
 
-    # URL для загрузки данных
     url = "https://raw.githubusercontent.com/likarajo/petrol_consumption/master/data/petrol_consumption.csv"
 
-    # Загрузка данных
     data = pd.read_csv(url)
 
-    # Подготовка данных
     X = data.drop('Petrol_Consumption', axis=1)
     y = data['Petrol_Consumption']
 
-    # Разделение данных на обучающий и тестовый наборы
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Создание и обучение модели регрессии
     model = LinearRegression()
     model.fit(X_train, y_train)
 
-    # Прогнозирование на тестовом наборе
     y_pred = model.predict(X_test)
 
-    # Оценка точности модели
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
     print(f'Среднеквадратичная ошибка (MSE): {mse:.2f}')
