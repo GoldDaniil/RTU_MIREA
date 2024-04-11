1b43820233c66310aa67bebccfb3e58084d80f8a
Daniil Gold <golddaniiil13@gmail.com>
2024-04-11 15:33:37 +0300

    Update main.py

--- Python/Discipline Artificial Intelligence/workbook_eight/main.py
+++ Python/Discipline Artificial Intelligence/workbook_eight/main.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn import datasets
 
-# Данные
 X = np.array([[5,3],
               [10,15],
               [15,12],
@@ -14,7 +14,7 @@ X = np.array([[5,3],
               [55,52],
               [80,91]])
 
-k_values = range(1, 6) 
+k_values = range(1, 6)  # Попробуем от 1 до 5 кластеров
 
 titles = [f'k={k}' for k in k_values]
 
@@ -25,8 +25,8 @@ for k, title, ax in zip(k_values, titles, axes):
     clusters = kmeans.fit_predict(X)
     ax.scatter(X[:, 0], X[:, 1], c=clusters, cmap='viridis', s=50, edgecolors='k')
     ax.set_title(title)
-    ax.set_xlabel('Feature 1')
-    ax.set_ylabel('Feature 2')
+    ax.set_xlabel('feature 1')
+    ax.set_ylabel('feature 2')
     ax.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', s=200, marker='X')
 plt.tight_layout()
 plt.show()
