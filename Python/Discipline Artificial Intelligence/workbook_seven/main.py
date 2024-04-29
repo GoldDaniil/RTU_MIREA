f837e5ff9c973c36543911fb37e5707aa846fe2e
dgold <golddaniiil13@gmail.com>
2024-04-29 09:21:04 +0300

    Update main.py

--- Python/Discipline Artificial Intelligence/workbook_seven/main.py
+++ Python/Discipline Artificial Intelligence/workbook_seven/main.py
@@ -59,17 +59,20 @@ def task_1_1():
     print("output using ReLU activation function:", output_relu)
 
 def task_1_2():
-    print('hello world')
+    print('hello world MLPClassified')
 
+def task_1_3():
+    print('hello world MLPRegressor')
 
 def main():
     while True:
         choice = input(
-            "\nselect a task to open: \n \n1 = task () \n2 = task () \n(no task - if you want to exit, enter 'exit'): ")
+            "\nselect a task to open: \n \n1 = task () \n2 = task() class MLPClassified \n3 = task() class MLPRegressor \n(no task - if you want to exit, enter 'exit'): ")
 
         menu = {
             '1': task_1_1,
             '2': task_1_2,
+            '3': task_1_3,
             'exit': lambda: print("oh, okay:(")
         }
 
