642a2f6c29dcf503717ad49bd8c9953a5991a546
dgold <golddaniiil13@gmail.com>
2024-09-08 21:28:51 +0300

    Create task8.sh

---
+++ Bash/pract_one/task8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "ЗАДАЧА 8"
+
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "использование: $0 <путь> <расширение>"
+        exit 1
+    fi
+
+    search_path="$1"
+    extension="$2"
+    archive_name="archive_$extension.tar"
+
+    if [ ! -d "$search_path" ]; then
+        echo "путь $search_path не существует или не является директорией"
+        exit 1
+    fi
+
+    echo "архивирование файлов с расширением $extension из $search_path в $archive_name..."
+    find "$search_path" -type f -name "*.$extension" | tar -cvf "$archive_name" -T -
+
+    echo "
+архив $archive_name создан"
