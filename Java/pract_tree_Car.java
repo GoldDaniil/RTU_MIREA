b670e3746ac1c4e997637ad714a3eaf92dd46dc6
dgold <golddaniiil13@gmail.com>
2024-09-28 23:00:09 +0300

    Create pract_tree_Car.java

---
+++ Java/pract_tree_Car.java
@@ -0,0 +1,50 @@
+package vehicles;
+
+public class Car {
+    private String ownerName;
+    private String insuranceNumber;
+    protected String engineType;
+
+    // конструктор
+    public Car(String ownerName, String insuranceNumber, String engineType) {
+        this.ownerName = ownerName;
+        this.insuranceNumber = insuranceNumber;
+        this.engineType = engineType;
+    }
+
+    // геттер для ownerName
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    // cеттер для ownerName
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    // геттер
+    public String getInsuranceNumber() {
+        return insuranceNumber;
+    }
+
+    // cеттер
+    public void setInsuranceNumber(String insuranceNumber) {
+        this.insuranceNumber = insuranceNumber;
+    }
+
+    // геттер 
+    public String getEngineType() {
+        return engineType;
+    }
+
+    // сеттер
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public void displayInfo() {
+        System.out.println("owner: " + ownerName);
+        System.out.println("insurance: " + insuranceNumber);
+        System.out.println("engine Type: " + engineType);
+    }
+}
