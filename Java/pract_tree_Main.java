9e6ab40129b2f5afb495563795dd4698ca0481fe
dgold <golddaniiil13@gmail.com>
2024-09-28 23:01:22 +0300

    Update and rename pract_tree_main.java to pract_tree_Main.java

---
+++ Java/pract_tree_Main.java
@@ -0,0 +1,19 @@
+package app;
+
+import vehicles.Car;
+import vehicles.ElectricCar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("John Doe", "12345ABC", "gasoline");
+        System.out.println("car Information:");
+        car.displayInfo();
+
+        System.out.println();
+
+        ElectricCar electricCar = new ElectricCar("Jane Doe", "98765XYZ", "electric", 85);
+        System.out.println("electric Car Information:");
+        electricCar.displayInfo();
+    }
+}
