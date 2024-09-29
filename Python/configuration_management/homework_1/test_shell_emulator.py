cb6f33016e50cb5c2ea8db519194d80084bb14f1
dgold <golddaniiil13@gmail.com>
2024-09-29 20:31:55 +0300

    Create test_shell_emulator.py

---
+++ Python/configuration_management/homework_1/test_shell_emulator.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestShellEmulator(unittest.TestCase):
+
+    def test_ls_command(self):
+        # Проверяем вывод ls
+        pass
+
+    def test_cd_command(self):
+        # Проверяем смену директории
+        pass
+
+    def test_exit_command(self):
+        # Проверяем завершение работы
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
