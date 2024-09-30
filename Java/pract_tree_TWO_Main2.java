704a609433711e6fff9d1a18fc4647aea1cab6d9
dgold <golddaniiil13@gmail.com>
2024-09-30 21:24:41 +0300

    Create pract_tree_TWO_Main2.java

---
+++ Java/pract_tree_TWO_Main2.java
@@ -0,0 +1,29 @@
+package app2;
+
+import vehicles2.Car;
+import vehicles2.ElectricCar;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Car car = new Car("alice", "98765QWERTY", "gasoline");
+        System.out.println("car Information:");
+        car.displayInfo();
+
+        System.out.println();
+
+        ElectricCar electricCar = new ElectricCar("bob", "12345ZXCVB", 150);
+        System.out.println("electric car Information:");
+        electricCar.displayInfo();
+
+        System.out.println();
+
+        System.out.println("accessing fields using methods:");
+        System.out.println("car owner: " + car.getOwnerName());
+        System.out.println("electric car owner: " + electricCar.getOwnerName());
+
+        System.out.println("electric car engine type: " + electricCar.getEngineType());
+
+        System.out.println("car insurance: " + car.getInsuranceNumber());
+    }
+}
