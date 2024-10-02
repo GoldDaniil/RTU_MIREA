ae7f7719459a744d633419956ae553b99e000cc8
dgold <golddaniiil13@gmail.com>
2024-10-02 11:45:44 +0300

    Create pract_fourElectricCar.java

---
+++ Java/pract_fourElectricCar.java
@@ -0,0 +1,24 @@
+package vehicles;
+
+public class ElectricCar extends Car {
+    private int batteryCapacity;
+
+    public ElectricCar(String model, String license, String color, int year, String ownerName, String insuranceNumber, int batteryCapacity) {
+        super(model, license, color, year, ownerName, insuranceNumber);
+        this.batteryCapacity = batteryCapacity;
+        this.engineType = "electric";
+    }
+
+    public int getBatteryCapacity() { return batteryCapacity; }
+    public void setBatteryCapacity(int batteryCapacity) { this.batteryCapacity = batteryCapacity; }
+
+    @Override
+    public String vehicleType() {
+        return "electric Car";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", battery capacity: " + batteryCapacity + " kWh";
+    }
+}
