7632c4a6d8261b525508982d143858a6c99a3902
dgold <golddaniiil13@gmail.com>
2024-10-31 21:17:40 +0300

    Update pract_four.py

--- Python/theory formal languages/pract_four.py
+++ Python/theory formal languages/pract_four.py
@@ -1,21 +1,29 @@
-Bin_num = ['1', '0', 'B', 'b']
-Oct_num = ['0', '1', '2', '3', '4', '5', '6', '7', 'O', 'o']
-Dec_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'D', 'd']
-Hex_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', 'H', 'h']
+bin_num = ['1', '0', 'B', 'b']
+oct_num = ['0', '1', '2', '3', '4', '5', '6', '7', 'O', 'o']
+dec_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'D', 'd']
+hex_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', 'H', 'h']
 real_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'e', 'E']
 keywords = ["program", "var", "begin", "end", "integer", "real", "boolean",
             "if", "then", "else", "for", "to", "do", "while", "read", "write",
             "as", "plus", "min", "or", "mult", "div", "and", "NE", "EQ", "LT",
             "LE", "GT", "GE"]
 
+delimiters = [',', ';', '(', ')', ':']
+
+#функция для обработки разделителей
+def process_delimiter(c):
+    if c in delimiters:
+        tok = token(Toknames.DELIM, c)
+        add_token(tok)
+
 class States:
-    H = "H"
-    ID = "ID"
-    NM = "NM"
-    ASGN = "ASGN"
-    DLM = "DLM"
-    COMM = "COMM"
-    ERR = "ERR"
+    H = "H"#начальное состояние
+    ID = "ID"#идентификатор
+    NM = "NM"#номер
+    ASGN = "ASGN"#присваивание
+    DLM = "DLM"#разделитель
+    COMM = "COMM"#комментарий
+    ERR = "ERR"#ошибка
 
 class Toknames:
     KWORD = "KWORD"
@@ -23,6 +31,7 @@ class Toknames:
     NUM = "NUM"
     OPER = "OPER"
     DELIM = "DELIM"
+    ERR = "ERR"#
 
 class token:
     def __init__(self, token_name, token_value):
@@ -44,13 +53,13 @@ def add_token(tok):
     global lt, lt_head
     new_entry = LexemeTable(tok)
     if lt is None:
-        lt = new_entry
+        lt = new_entry#первый токен добавляется в таблицу лексем
         lt_head = new_entry
     else:
         lt.next = new_entry
         lt = new_entry
 
-def lexer(filename):
+def lexer(filename):#лекс анализ файла
     try:
         with open(filename, "r") as fd:
             CS = States.H
@@ -59,7 +68,7 @@ def lexer(filename):
                 if CS == States.H:
                     while c in [' ', '\t', '\n']:
                         c = fd.read(1)
-                    if c.isalpha() or c == '_':
+                    if c.isalpha():
                         CS = States.ID
                     elif c.isdigit() or c == '.':
                         CS = States.NM
@@ -124,23 +133,36 @@ def lexer(filename):
                         tok = token(Toknames.IDENT, buf)
                     add_token(tok)
                     CS = States.H
+
                 elif CS == States.NM:
                     buf = ''
-                    while c.isdigit() or c in ['B', 'b', 'O', 'o', 'D', 'd', 'H', 'h', '.']:
+                    while c.isdigit() or c in ['B', 'O', 'D', 'H', '.', 'b', 'o', 'd', 'h']:
                         buf += c
                         c = fd.read(1)
-                    tok = token(Toknames.NUM, buf)
+
+                    #проверка на валидность формата числа
+                    if buf[-1].lower() in ['b', 'o', 'd', 'h'] and buf[:-1].isdigit():
+                        tok = token(Toknames.NUM, buf)
+                    elif buf.isdigit() or '.' in buf:
+                        tok = token(Toknames.NUM, buf)
+                    else:
+                        tok = token(Toknames.ERR, buf)#формат нен слслответсувует числу
+
                     add_token(tok)
                     CS = States.H
 
     except FileNotFoundError:
         print(f"\ncannot open file {filename}.\n")
+
         return -1
+
 
 def print_tokens(lt_head):
     current = lt_head
+
     while current:
         print(f"token name: {current.tok.token_name}, token value: {current.tok.token_value}")
+
         current = current.next
 
 def main():
