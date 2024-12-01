a1314f9a6b9236c4a5a5a2d19675c87a15cb2746
dgold <golddaniiil13@gmail.com>
2024-12-01 17:28:17 +0300

    Update shell_gui.py

--- Python/configuration_management/homework1/shell_gui.py
+++ Python/configuration_management/homework1/shell_gui.py
@@ -6,7 +6,7 @@ class ShellGUI:
     def __init__(self, root, shell):
         self.root = root
         self.shell = shell
-        self.root.title(f"{self.shell.user}@{self.shell.host}")
+        self.root.title(f"{self.shell.user}@{self.shell.host}")#доступ к атрибутам shell
         self.root.geometry("800x600")
 
         self.text_font = font.nametofont('TkTextFont')
@@ -15,7 +15,7 @@ class ShellGUI:
         self.entry_font = font.nametofont('TkFixedFont')
         self.entry_font.config(size=14)
 
-        self.root.configure(bg='#2e2e2e')#фон и стиль
+        self.root.configure(bg='#2e2e2e')
 
         self.output_frame = tk.Frame(root, bg='#333333', padx=10, pady=10)
         self.output_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=10)
@@ -23,9 +23,9 @@ class ShellGUI:
         self.output_text = tk.Text(self.output_frame, height=15, width=80, bg='#1e1e1e', fg='white',
                                    font=self.text_font, wrap=tk.WORD, bd=0, padx=10, pady=10)
         self.output_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.output_text.config(state=tk.DISABLED)#заперт редактирование
+        self.output_text.config(state=tk.DISABLED)#запрет на редактирование
 
-        self.input_frame = tk.Frame(root, bg='#2e2e2e', padx=10, pady=10)#gанель для ввода
+        self.input_frame = tk.Frame(root, bg='#2e2e2e', padx=10, pady=10)#панель для ввода
         self.input_frame.pack(fill=tk.X, padx=20, pady=10)
 
         self.input_entry = tk.Entry(self.input_frame, width=80, font=self.entry_font, bd=0, fg='white',
@@ -40,21 +40,21 @@ class ShellGUI:
         self.input_entry.delete(0, tk.END)
         result = self.shell.execute(command)
 
-        #редакт + добав рез + блок редакт
+        # редакт + добав рез + блок редакт
         self.output_text.config(state=tk.NORMAL)
 
         if command.startswith("ls"):
-            color = "#00FF00" 
+            color = "#00FF00"
         elif command.startswith("cd"):
             color = "#FFA500"
         elif command.startswith("who"):
-            color = "#ADD8E6"  
+            color = "#ADD8E6"
         elif command.startswith("mv"):
             color = "#FFFF00"
         elif command.startswith("wc"):
-            color = "#FF6347"  
+            color = "#FF6347"
         else:
-            color = "#FFFFFF"  
+            color = "#FFFFFF"
 
         self.output_text.insert(tk.END, f"$ {command}\n", "command")
 
@@ -68,3 +68,8 @@ class ShellGUI:
 
     def run(self):
         self.root.mainloop()
+
+    def clear_gui_output(self):
+        self.output_text.config(state=tk.NORMAL)
+        self.output_text.delete(1.0, tk.END)
+        self.output_text.config(state=tk.DISABLED)
