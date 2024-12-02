57eb87a9f518a0597d06d428d9845dc2a619e57e
dgold <golddaniiil13@gmail.com>
2024-12-02 23:47:47 +0300

    Create pract_thirteen_BlueColor.java

---
+++ Java/pract_thirteen_BlueColor.java
@@ -0,0 +1,8 @@
+package third;
+
+public class BlueColor implements DrawAPI {
+    @Override
+    public void drawShape(String shapeType, int x, int y, int size) {
+        System.out.println("drawing " + shapeType + " in blue at (" + x + ", " + y + ") with size " + size);
+    }
+}
