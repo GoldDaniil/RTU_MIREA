7dd40767b0623c45da4adb4794a3b189bc63b315
dgold <golddaniiil13@gmail.com>
2024-12-02 23:52:26 +0300

    Create Square.java

---
+++ Java/Square.java
@@ -0,0 +1,22 @@
+package third;
+
+public class Square extends Shape {
+    private int x, y, sideLength;
+
+    public Square(int x, int y, int sideLength, DrawAPI drawAPI) {
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public void draw() {
+        drawAPI.drawShape("square", x, y, sideLength);
+    }
+
+    @Override
+    public String toString() {
+        return "square at (" + x + ", " + y + "), side length: " + sideLength;
+    }
+}
