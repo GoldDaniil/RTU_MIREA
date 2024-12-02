58ce189ab470c95b8f26cbbcdcf93b6718254af6
dgold <golddaniiil13@gmail.com>
2024-12-02 23:51:43 +0300

    Create pract_thirteen_Circle.java

---
+++ Java/pract_thirteen_Circle.java
@@ -0,0 +1,22 @@
+package third;
+
+public class Circle extends Shape {
+    private int x, y, radius;
+
+    public Circle(int x, int y, int radius, DrawAPI drawAPI) {
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        drawAPI.drawShape("circle", x, y, radius);
+    }
+
+    @Override
+    public String toString() {
+        return "circle at (" + x + ", " + y + "), radius: " + radius;
+    }
+}
