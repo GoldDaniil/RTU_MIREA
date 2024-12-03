9180d67f1b10180ad294be64aafccde004dd18ad
dgold <golddaniiil13@gmail.com>
2024-12-04 00:23:58 +0300

    Create Bike.java

---
+++ Java/homework/pract_six/Bike.java
@@ -0,0 +1,33 @@
+package bikeproject;
+
+public class Bike {
+    private String handleBars;
+    private String frame;
+    private String tyres;
+    private String seatType;
+    private int NumGears;
+    private final String make;
+
+    public Bike() {
+        this.handleBars = "default handlebars";
+        this.frame = "default frame";
+        this.tyres = "default tyres";
+        this.seatType = "default seat";
+        this.NumGears = 1;
+        this.make = "Oracle Cycles";
+    }
+
+
+    public Bike(String handleBars, String frame, String tyres, String seatType, int numGears) {
+        this.handleBars = handleBars;
+        this.frame = frame;
+        this.tyres = tyres;
+        this.seatType = seatType;
+        this.NumGears = numGears;
+        this.make = "Oracle Cycles";
+    }
+
+    protected void printDescription() {
+        System.out.println("\n" + this.make + "\n" + "this bike has " + this.handleBars + " handlebars on a " + this.frame + " frame with " + this.NumGears + " gears." + "\nIt has a " + this.seatType + " seat with " + this.tyres + " tyres");
+    }
+}
