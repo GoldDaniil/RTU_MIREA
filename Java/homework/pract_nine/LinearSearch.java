27aff0881fd573b38a9a562cbfcceb6519ba4a66
dgold <golddaniiil13@gmail.com>
2024-12-04 00:39:51 +0300

    Create LinearSearch.java

---
+++ Java/homework/pract_nine/LinearSearch.java
@@ -0,0 +1,10 @@
+class LinearSearch {
+    public static int findElement(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
