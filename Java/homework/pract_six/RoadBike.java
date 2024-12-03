a06446e3a03300ff33ec577c7f503aee083813b9
dgold <golddaniiil13@gmail.com>
2024-12-04 00:23:17 +0300

    Update and rename Java/pract_six.java to Java/homework/pract_six/RoadBike.java

---
+++ Java/homework/pract_six/RoadBike.java
@@ -0,0 +1,28 @@
+package bikeproject;
+
+public class RoadBike extends Bike {
+    private int tyreWidth;
+    private int postHeight;
+
+    public RoadBike() {
+        this("drop", "racing", "tread less", "razor", 19, 20, 22);
+    }
+
+    public RoadBike(int postHeight) {
+        this("drop", "racing", "tread less", "razor", 19, 20, postHeight);
+    }
+
+    public RoadBike(String handleBars, String frame, String tyres, String seatType, int numGears, int tyreWidth, int postHeight) {
+        super(handleBars, frame, tyres, seatType, numGears);
+        this.tyreWidth = tyreWidth;
+        this.postHeight = postHeight;
+    }
+
+    @Override
+    public void printDescription() {
+        super.printDescription();
+        System.out.println("this Roadbike bike has " + this.tyreWidth + "mm tyres and a post height of " + this.postHeight);
+    }
+}
+
+
