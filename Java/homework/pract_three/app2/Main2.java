cfe48dd22513ba58d46ac336a0ef52014bbd63e4
dgold <golddaniiil13@gmail.com>
2024-12-04 00:19:30 +0300

    Update and rename Java/pract_tree_TWO_Main2.java to Java/homework/pract_three/app2/Main2.java

--- Java/homework/pract_three/app2/Main2.java
+++ Java/homework/pract_three/app2/Main2.java
@@ -7,13 +7,13 @@ public class Main2 {
 
     public static void main(String[] args) {
         Car car = new Car("alice", "98765QWERTY", "gasoline");
-        System.out.println("car Information:");
+        System.out.println("car information:");
         car.displayInfo();
 
         System.out.println();
 
         ElectricCar electricCar = new ElectricCar("bob", "12345ZXCVB", 150);
-        System.out.println("electric car Information:");
+        System.out.println("electric car information:");
         electricCar.displayInfo();
 
         System.out.println();
