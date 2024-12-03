59f5643a344f4e571925021d2be1ed12528d8758
dgold <golddaniiil13@gmail.com>
2024-12-04 00:13:57 +0300

    Update and rename Java/pract_tree_ONE_Main.java to Java/homework/pract_three/Main.java

--- Java/homework/pract_three/Main.java
+++ Java/homework/pract_three/Main.java
@@ -6,13 +6,13 @@ import vehicles.ElectricCar;
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car("John Doe", "12345ABC", "gasoline");
-        System.out.println("car Information:");
+        Car car = new Car("john", "12345ABC", "gasoline");
+        System.out.println("car information:");
         car.displayInfo();
 
         System.out.println();
 
-        ElectricCar electricCar = new ElectricCar("Jane Doe", "98765XYZ", "electric", 85);
+        ElectricCar electricCar = new ElectricCar("jane", "98765XYZ", "electric", 85);
         System.out.println("electric car information:");
         electricCar.displayInfo();
     }
