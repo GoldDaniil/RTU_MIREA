10f1b9382377d3cc92d6703e9f9e653c51737c0c
dgold <golddaniiil13@gmail.com>
2024-12-04 00:24:18 +0300

    Create MountainBike.java

---
+++ Java/homework/pract_six/MountainBike.java
@@ -0,0 +1,24 @@
+package bikeproject;
+
+public class MountainBike extends Bike {
+    private String suspension;
+    private String type;
+    private int frameSize;
+
+    public MountainBike() {
+        this("Bull Horn", "Hardtail", "Maxxis", "dropper", 27, "RockShox XC32", "Pro", 19);
+    }
+
+    public MountainBike(String handleBars, String frame, String tyres, String seatType, int numGears, String suspension, String type, int frameSize) {
+        super(handleBars, frame, tyres, seatType, numGears);
+        this.suspension = suspension;
+        this.type = type;
+        this.frameSize = frameSize;
+    }
+
+    @Override
+    public void printDescription() {
+        super.printDescription();
+        System.out.println("this mountain bike is a " + this.type + " bike and has a " + this.suspension + " suspension and a frame size of " + this.frameSize + " inches");
+    }
+}
