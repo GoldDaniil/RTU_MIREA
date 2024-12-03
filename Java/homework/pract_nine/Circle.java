f52e5bc13c39c5545d2c48dcec5b7513b8c48477
dgold <golddaniiil13@gmail.com>
2024-12-04 00:40:06 +0300

    Create Circle.java

---
+++ Java/homework/pract_nine/Circle.java
@@ -0,0 +1,21 @@
+class Circle implements Comparable<Circle> {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public int compareTo(Circle other) {
+        return Double.compare(this.radius, other.radius);
+    }
+
+    @Override
+    public String toString() {
+        return "circle with radius: " + radius;
+    }
+}
