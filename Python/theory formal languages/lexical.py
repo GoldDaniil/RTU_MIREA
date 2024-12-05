0a74c0ce28314442eb0c0866cc1fc4dfa93e34fa
dgold <golddaniiil13@gmail.com>
2024-12-05 22:39:24 +0300

    Update lexical.py

--- Python/theory formal languages/lexical.py
+++ Python/theory formal languages/lexical.py
@@ -28,69 +28,67 @@ def out(n, k, lexem_value, token_name, filename="lexems.txt"):
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
 def lexical_analysis():
-    #def get_unique_input(prompt, used_values, allow_duplicates=False):
-    #    while True:
-    #        value = input(prompt).strip()
-    #        if allow_duplicates or value not in used_values:
-    #            used_values.add(value)
-    #           return value
-    #        print(f"!ошибка: символ '{value}' уже занят! введите другой!")
+    def get_unique_input(prompt, used_values, allow_duplicates=False):
+        while True:
+            value = input(prompt).strip()
+            if allow_duplicates or value not in used_values:
+                used_values.add(value)
+                return value
+            print(f"!ошибка: символ '{value}' уже занят! введите другой!")
 
-    #used_values = set()
+    used_values = set()
 
-    #output_symbol = get_unique_input("введите символ, который будет выходным - например : '}': ", used_values)
-    #comment_start = get_unique_input("введите символ начала комментария -н апример : '/': ", used_values, allow_duplicates=True)
-    #comment_end = get_unique_input("введите символ конца комментария - например : '*': ", used_values, allow_duplicates=True)
-    #transition_symbol = get_unique_input("введите связующий символ между C1, C2 и C3 = например : 'A': ", used_values)
+    output_symbol = get_unique_input("введите символ, который будет выходным - наприме  р : '}': ", used_values)
+    comment_start = get_unique_input("введите символ начала комментария -н апример : '/': ", used_values, allow_duplicates=True)
+    comment_end = get_unique_input("введите символ конца комментария - например : '*': ", used_values, allow_duplicates=True)
+    transition_symbol = get_unique_input("введите связующий символ между C1, C2 и C3 = например : 'A': ", used_values)
 
-    #m1_symbols = get_unique_input("введите символы для M1 - например : '< <= !=': ").split()
-    #m2_symbols = get_unique_input("введите символы для M2 - например : '> >=': ").split()
-
-    #operations_input = get_unique_input("введите символы для группы операций (сложение вычитание дизъюнкция): ")
-    #operations_list = operations_input.split()
-
-    output_symbol = input("введите символ, который будет выходным - например : '}': ")
-    comment_start = input("введите символ начала комментария -н апример : '/': ")
-    comment_end = input("введите символ конца комментария - например : '*': ")
-    transition_symbol = input("введите связующий символ между C1, C2 и C3 = например : 'A': ")
     m1_symbols = input("введите символы для M1 - например : '< <= !=': ").split()
+    for symbol in m1_symbols:
+        get_unique_input(f"проверка символа '{symbol}' для M1: ", used_values)
+
     m2_symbols = input("введите символы для M2 - например : '> >=': ").split()
+    for symbol in m2_symbols:
+        get_unique_input(f"проверка символа '{symbol}' для M2: ", used_values)
 
-    operations_input = input("введите символы для группы операций (сложение вычитание дизъюнкция): ")
-    operations_list = operations_input.split()
+    operations_input = input("введите символы для группы операций (сложение вычитание дизъюнкция): ").split()
 
-    if len(operations_list) < 3:
-        print("ошибка: введите как минимум три символа (сложения вычитания дизъюнкции)")
+    if len(operations_input) < 3:
+        print("!Ошибка: введите как минимум три символа (сложение, вычитание, дизъюнкция).")
         return
+    for symbol in operations_input:
+        get_unique_input(f"проверка символа '{symbol}' для операций: ", used_values)
 
     operation_tokens = {
-        'сложение': operations_list[0],
-        'вычитание': operations_list[1],
-        'дизъюнкция': operations_list[2]
+        'сложение': operations_input[0],
+        'вычитание': operations_input[1],
+        'дизъюнкция': operations_input[2]
     }
 
-    print("Распознанные операции:")
+    print("!распознанные операции:")
     for operation, token in operation_tokens.items():
         print(f"{operation}: {token}")
 
     #------------------------------------------------------------------------------------------------------------------------------------------------
 
-    operations_input_multiplication = input("введите символы для группы операций (умножение, деление, конъюнкция): ")
-    operations_list_multiplication = operations_input_multiplication.split()
-    if len(operations_list_multiplication) < 3:
+    operations_input_multiplication = input("введите символы для группы операций (умножение, деление, конъюнкция): ").split()
+    if len(operations_input_multiplication) < 3:
         print("ошибка: введите как минимум три символа (умножение, деление, конъюнкция)")
         return
+    for symbol in operations_input_multiplication:
+        get_unique_input(f"Проверка символа '{symbol}' для операций умножения: ", used_values)
 
-    operation_tokens_multiplication= {
-        'умножение': operations_list_multiplication[0],
-        'деление': operations_list_multiplication[1],
-        'конъюнкция': operations_list_multiplication[2]
+    operation_tokens_multiplication = {
+        'умножение': operations_input_multiplication[0],
+        'деление': operations_input_multiplication[1],
+        'конъюнкция': operations_input_multiplication[2]
     }
 
-    print("!Распознанные операции:")
+    print("!распознанные операции:")
     for operation_multiplication, token_multiplication in operation_tokens_multiplication.items():
         print(f"{operation_multiplication}: {token_multiplication}")
 
+    print("\n!!все входные данные успешно введены и проверены на уникальность")
 
 
     #------------------------------------------------------------------------------------------------------------------------------------------------
@@ -108,7 +106,7 @@ def lexical_analysis():
         if state == "H":
             if char == comment_start:
                 state = "C1"
-                print(f"!Переход в состояние C1: найден символ начала комментария '{comment_start}'")
+                print(f"!переход в состояние C1: найден символ начала комментария '{comment_start}'")
 
             elif char == '0' or char == '1':
                 buffer += char
@@ -125,9 +123,9 @@ def lexical_analysis():
                 print(f"коммент end - возврат в состояние H")
             elif char == transition_symbol:
                 state = "C2" 
-                print(f"!Переход в состояние C2: найден символ '{transition_symbol}'")
+                print(f"!переход в состояние C2: найден символ '{transition_symbol}'")
             else:
-                print(f"Считываем символ в состоянии C1: {char}")
+                print(f"считываем символ в состоянии C1: {char}")
 
         elif state == "C2":
             if char == comment_end:
@@ -135,16 +133,16 @@ def lexical_analysis():
                 print(f"коммент end - возврат в состояние H")
             elif char == transition_symbol:
                 state = "C3"  
-                print(f"!Переход в состояние C3: найден символ '{transition_symbol}'")
+                print(f"!переход в состояние C3: найден символ '{transition_symbol}'")
             else:
-                print(f"Считываем символ в состоянии C2: {char}")
+                print(f"считываем символ в состоянии C2: {char}")
 
         elif state == "C3":
             if char == transition_symbol:
                 state = "H" 
-                print(f"!Переход в состояние H: найден символ '{transition_symbol}'")
+                print(f"!переход в состояние H: найден символ '{transition_symbol}'")
             else:
-                print(f"Считываем символ в состоянии C3: {char}")
+                print(f"считываем символ в состоянии C3: {char}")
             if char == comment_end:
                 state = "H" 
                 print(f"коммент end - возврат в состояние H")
@@ -154,7 +152,7 @@ def lexical_analysis():
         process_lexeme(buffer, m1_symbols, m2_symbols, operation_tokens, operation_tokens_multiplication)
 
 def process_lexeme(buffer, m1_symbols, m2_symbols, operation_tokens, operation_tokens_multiplication):
-    print(f"\n!Обрабатываем лексему: {buffer}")
+    print(f"\n!!!обрабатываем лексему: {buffer}")
 
     if buffer in TW:#служебное слово
         z = TW.index(buffer) + 1
@@ -169,6 +167,24 @@ def process_lexeme(buffer, m1_symbols, m2_symbols, operation_tokens, operation_t
             TN.append(buffer)
         z = TN.index(buffer) + 1
         out(4, z, buffer, "NUM")
+
+    elif (
+            buffer.replace('.', '', 1).isdigit() or
+            (buffer.startswith('-') and buffer[1:].replace('.', '', 1).isdigit())
+    ) and buffer.count('.') == 1:
+        parts = buffer.split('.')
+        whole_part = parts[0]
+        fractional_part = parts[1] if len(parts) > 1 and parts[1] else None
+        if fractional_part == "0":
+            if whole_part not in TN:
+                TN.append(whole_part)
+            z = TN.index(whole_part) + 1
+            out(4, z, buffer, "P1")
+        else:
+            if buffer not in TN:
+                TN.append(buffer)
+            z = TN.index(buffer) + 1
+            out(4, z, buffer, "P2")
 
     elif buffer in Operators:#oператоры
         z = Operators.index(buffer) + 1
@@ -202,25 +218,23 @@ def process_lexeme(buffer, m1_symbols, m2_symbols, operation_tokens, operation_t
             out(3, z, buffer, "IDENT")
 
     elif buffer[0] in "01" and (buffer[-1] == 'b' or buffer[-1] == 'B') and all(c in "01" for c in buffer[:-1]):
-        z = 8#примерный индекс для бинарного числа
+        z = 8#примерный индекс
         out(4, z, buffer, "BIN")
 
     elif all(c in "01234567" for c in buffer[:-1]) and (buffer[-1].lower() == 'o'):
-        z = 9#примерный индекс для восьмеричного числа
+        z = 9#примерный индекс
         out(4, z, buffer, "OCT")
 
     elif all(c in "0123456789" for c in buffer[:-1]) and (buffer[-1].lower() == 'd'):
-        z = 10#примерный индекс для десятичного числа
+        z = 10#примерный индекс
         out(4, z, buffer, "DEC")
 
     elif buffer[0] in "0123456789" and all(c in "0123456789ABCDEFabcdef" for c in buffer[1:-1]) and (buffer[-1].lower() == 'h'):
-        z = 16#примерный индекс для шестнадцатеричного числа
+        z = 16#примерный индекс числа
         out(4, z, buffer, "HEX")
 
     else:
-        print(f"Неизвестная лексема: {buffer}")
-
-
+        print(f"неизвестная лексема: {buffer}")
 
 def is_binary_number(buffer):
     if all(c in '01' for c in buffer[:-1]) and (buffer[-1].lower() == 'b' or buffer[-1] == ''):
