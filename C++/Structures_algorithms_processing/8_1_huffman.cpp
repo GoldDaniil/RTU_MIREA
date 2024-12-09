833388e1b75ae100cea0ac688d2856ddc59da0ca
dgold <golddaniiil13@gmail.com>
2024-12-09 20:31:59 +0300

    Update 8_1_huffman.cpp

--- C++/Structures_algorithms_processing/8_1_huffman.cpp
+++ C++/Structures_algorithms_processing/8_1_huffman.cpp
@@ -1,105 +1,131 @@
 #include <iostream>
-#include <vector>
-#include <map>
 #include <string>
+#include <map>
+#include <queue>
+#include <vector>
 #include <algorithm>
-#include <locale>
-#include <codecvt>
 
 using namespace std;
 
-struct Node {//узел дерева шеннона-фано
+struct huffman_node {//узел дерева хаффмана
     char symbol;
     double symbol_frequency;
-    string character_code;
+    huffman_node* left_child;
+    huffman_node* right_child;
 };
 
-bool compare_frequency(Node tree_nodes_a, Node tree_nodes_b) {//сравниваем частот для сортировоки
-    return tree_nodes_a.symbol_frequency > tree_nodes_b.symbol_frequency;
+struct compare {//компаратор для приоритетной очереди
+    bool operator()(huffman_node* tree_nodes_a, huffman_node* tree_nodes_b) {
+        return tree_nodes_a->symbol_frequency > tree_nodes_b->symbol_frequency;
+    }
+};
+
+void generate_huffman_codes(huffman_node* node, string current_code, map<char, string>& huffman_codes) {
+    if (!node) return;
+    //рекурсивная функция для генерации кодов символов
+
+    if (!node->left_child && !node->right_child) {//если узел листовой - добавляем символ и его код
+        huffman_codes[node->symbol] = current_code;
+    }
+
+    generate_huffman_codes(node->left_child, current_code + "0", huffman_codes);//рекурсивно вызываем для левого и правого поддерева
+    generate_huffman_codes(node->right_child, current_code + "1", huffman_codes);
 }
 
-void generate_codes(vector<Node>& symbol_nodes, int start, int end, string character_code) {//рекурс функ - построение кодов
-    if (start == end) {
-        symbol_nodes[start].character_code = character_code;
-        return;
-    }
-
-    double sum_symbol_frequency_left = 0, sum_symbol_frequency_right = 0;
-    int left_froup_end_index = start;
-
-    for (int i = start; i <= end; ++i) sum_symbol_frequency_right += symbol_nodes[i].symbol_frequency;
-
-    for (int i = start; i <= end; ++i) {
-        sum_symbol_frequency_left += symbol_nodes[i].symbol_frequency;
-        sum_symbol_frequency_right -= symbol_nodes[i].symbol_frequency;
-
-        if (sum_symbol_frequency_left >= sum_symbol_frequency_right) {
-            left_froup_end_index = i;
-            break;
-        }
-    }
-
-    generate_codes(symbol_nodes, start, left_froup_end_index, character_code + "0");
-    generate_codes(symbol_nodes, left_froup_end_index + 1, end, character_code + "1");
+void delete_huffman_tree(huffman_node* node) {//рекурсивное удаление дерева
+    if (!node) return;
+    delete_huffman_tree(node->left_child);
+    delete_huffman_tree(node->right_child);
+    delete node;
 }
 
 int main() {
-    setlocale(LC_ALL, "");
+    string input_text = "Gold Daniil Vladimirovich";
 
-    //string input = "Эни-бени_рити-Фати._Дорба,_дорба_сентибрати._Дэл._Дэл._Кошка._Дэл._Фати!";
-    //э = q ; Э = Q ; ш = s;  
-
-    string input = "Qni-beni riti-Fati. Dorba, dorba centibrati. Dql. Dql. Koska. Dql. Fati!";
-
-    map<char, double> frequency_map;//частота симв
-    for (char c : input) {
+    map<char, double> frequency_map;
+    for (char c : input_text) {
         frequency_map[c]++;
     }
 
-    vector<Node> symbol_nodes;
+    priority_queue<huffman_node*, vector<huffman_node*>, compare> priority_queue_huffman_nodes;//создание приоритетной очереди
     for (auto pair : frequency_map) {
-        symbol_nodes.push_back({ pair.first, pair.second / input.size(), "" });
+        huffman_node* new_huffman_node = new huffman_node{ pair.first, pair.second, nullptr, nullptr };
+        priority_queue_huffman_nodes.push(new_huffman_node);
     }
 
-    sort(symbol_nodes.begin(), symbol_nodes.end(), compare_frequency);
+    while (priority_queue_huffman_nodes.size() > 1) {//построение дерева хаффмана
+        huffman_node* left_child = priority_queue_huffman_nodes.top(); priority_queue_huffman_nodes.pop();
+        huffman_node* right_child = priority_queue_huffman_nodes.top(); priority_queue_huffman_nodes.pop();
 
-    generate_codes(symbol_nodes, 0, symbol_nodes.size() - 1, "");//строим дерево
-
-    map<char, string> codes;
-    for (auto node : symbol_nodes) {
-        codes[node.symbol] = node.character_code;
-        cout << node.symbol << " -> " << node.character_code << endl;
+        huffman_node* merged = new huffman_node{ '\0', left_child->symbol_frequency + right_child->symbol_frequency,
+            left_child, right_child };
+        priority_queue_huffman_nodes.push(merged);
     }
 
-    string encoded = "";//кодирование
-    for (char c : input) {
-        encoded += codes[c];
+    huffman_node* root = priority_queue_huffman_nodes.top();//корень дерева хаффмана
+    map<char, string> huffman_codes;//генерация кодов символов
+    generate_huffman_codes(root, "", huffman_codes);
+
+    cout << "symbols codes:\n";//отображение кодов символов
+    for (auto pair : huffman_codes) {
+        cout << pair.first << " -> " << pair.second << endl;
     }
 
-    cout << "encoded text: " << encoded << endl;
+    string encoded_text = "";
+    for (char c : input_text) {
+        encoded_text += huffman_codes[c];
+    }
+    cout << "\nencoded text: " << encoded_text << endl;
 
-    //cжатый текст
-    int original_size_bits = input.size() * 8;//исходный размер в битах
-    int compressed_size_bits = encoded.size();//сжатый размер в битах
+    //сжатый текст
+    int original_size_bits = input_text.size() * 8;//исходный размер в битах (по 8 бит на символ)
+    int compressed_size_bits = encoded_text.size();//сжатый размер в битах
 
-    double compression_ratio = (double)compressed_size_bits / original_size_bits * 100;//процент сжатия
+    //коэффициенты сжатия
+    double compression_ratio_ascii = (double)original_size_bits / compressed_size_bits;
+    double average_code_length = 0.0;
+    double variance = 0.0;
 
-    string decoded = "";//декодирование
-    string temp = "";
-    for (char bit : encoded) {
-        temp += bit;
-        for (auto node : symbol_nodes) {
-            if (node.character_code == temp) {
-                decoded += node.symbol;
-                temp = "";
-                break;
-            }
+    for (auto pair : huffman_codes) {//расчет средней длины кода и дисперсии
+        int code_length = pair.second.size();
+        average_code_length += code_length * (pair.second.size() / (double)input_text.size());
+    }
+
+    for (auto pair : huffman_codes) {//вычисление дисперсии длины кодов
+        int code_length = pair.second.size();
+        variance += (code_length - average_code_length) * (code_length - average_code_length) *
+            (pair.second.size() / (double)input_text.size());
+    }
+
+    //коэффициент сжатия относительно равномерного кода
+    int uniform_code_size = (int)ceil(log2(frequency_map.size())) * input_text.size();//равномерный код
+    double compression_ratio_uniform = (double)original_size_bits / uniform_code_size;
+
+    cout << "\ncompressed size in bits: " << compressed_size_bits << endl;
+    cout << "compression ratio (ASCII): " << compression_ratio_ascii << endl;
+    cout << "compression ratio (uniform code): " << compression_ratio_uniform << endl;
+    cout << "average code length: " << average_code_length << " bits\n";
+    cout << "variance code length: " << variance << endl;
+
+    string decoded_text = "";//декодирование текста
+    huffman_node* current_node = root;
+    for (char bit : encoded_text) {
+        if (bit == '0') {
+            current_node = current_node->left_child;
+        }
+        else {
+            current_node = current_node->right_child;
+        }
+
+        if (!current_node->left_child && !current_node->right_child) {
+            decoded_text += current_node->symbol;
+            current_node = root;
         }
     }
 
-    cout << "\ncompressed text in bits: " << compressed_size_bits << endl;
-    cout << "compression ratio: " << compression_ratio << "%\n";
-    cout << "\ndecoded text: " << decoded << endl;
+    cout << "\ndecoded text: " << decoded_text << endl;
+
+    delete_huffman_tree(root);
 
     return 0;
 }
