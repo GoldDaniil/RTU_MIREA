b15c701f2965aace574fc68d092ed5854e5e4120
dgold <golddaniiil13@gmail.com>
2024-12-09 22:22:28 +0300

    Update main.cpp

--- C++/procedural_programming_1_1/homework/homework_one/main.cpp
+++ C++/procedural_programming_1_1/homework/homework_one/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
-
-using namespace std;
+#include <string>
 
 void display_menu();
+
+void handleMenuChoice(int choice);
+
 void print_name();
-void sum_difference_product();
+void calculate_sum_difference_product();
 void solve_linear_equation();
 void solve_quadratic_equation();
+
 void clear_input();
 
 int main() {
@@ -16,132 +19,140 @@ int main() {
 
     while (true) {
         display_menu();
-
-        if (!(cin >> choice)) {
-            cout << "ошибка ввода - введите число от 0 до 4\n";
+        if (!(std::cin >> choice)) {
+            std::cout << "ошибка ввода: введите число от 0 до 4\n";
             clear_input();
             continue;
         }
 
-        switch (choice) {
-        case 1: print_name(); break;
-        case 2: sum_difference_product(); break;
-        case 3: solve_linear_equation(); break;
-        case 4: solve_quadratic_equation(); break;
-        case 0: cout << "ну зачем...\n"; return 0;
-        default: cout << "от 0 до 4 ток\n";
+        if (choice == 0) {
+            std::cout << "зачем...\n";
+            return 0;
         }
+
+        handleMenuChoice(choice);
     }
 }
 
 void display_menu() {
-    cout << "\n=== лаунчер ===\n";
-    cout << "1 - вывести имя\n";
-    cout << "2 - сумма - разность - произведение\n";
-    cout << "3 - решить линейное уравнение\n";
-    cout << "4 - решить квадратное уравнение\n";
-    cout << "0 - выход\n";
-    cout << "выберите задачу: ";
+    std::cout << "\n=== лаунчер ===\n";
+    std::cout << "1 - вывести имя\n";
+    std::cout << "2 - сумма, разность, произведение\n";
+    std::cout << "3 - решить линейное уравнение\n";
+    std::cout << "4 - решить квадратное уравнение\n";
+    std::cout << "0 - выход\n";
+    std::cout << "выберите задачу: ";
+}
+
+void handleMenuChoice(int choice) {
+    switch (choice) {
+    case 1: print_name(); break;
+    case 2: calculate_sum_difference_product(); break;
+    case 3: solve_linear_equation(); break;
+    case 4: solve_quadratic_equation(); break;
+    default:
+        std::cout << "ошибка: введите число от 0 до 4\n";
+        break;
+    }
 }
 
 void print_name() {
-    string my_name = "asdasd";
-    cout << "\nваше имя: " << my_name << "\n\n";
+    const std::string name = "asdasd";
+    std::cout << "\nваше имя: " << name << "\n\n";
 }
 
-void sum_difference_product() {
+void calculate_sum_difference_product() {
     double num1, num2;
 
-    cout << "\n--- сумма, разность, произведение ---\n";
-    cout << ": ";
-    while (!(cin >> num1)) {
-cout << "error!
-";
+    std::cout << "\n--- сумма, разность, произведение ---\n";
+    std::cout << "введите первое число: ";
+    while (!(std::cin >> num1)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
-    cout << "введите второе число: ";
-    while (!(cin >> num2)) {
-cout << "error!
-";
+    std::cout << "введите второе число: ";
+    while (!(std::cin >> num2)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
-    cout << "сумма: " << num1 + num2 << "\n";
-    cout << "разность: " << num1 - num2 << "\n";
-    cout << "произведение: " << num1 * num2 << "\n";
+    std::cout << "сумма: " << num1 + num2 << "\n";
+    std::cout << "разность: " << num1 - num2 << "\n";
+    std::cout << "произведение: " << num1 * num2 << "\n";
 
     if (num2 != 0) {
-        cout << "частное: " << num1 / num2 << "\n\n";
+        std::cout << "частное: " << num1 / num2 << "\n\n";
     }
     else {
-        cout << "деление на ноль невозможно\n\n";
+        std::cout << "деление на ноль невозможно\n\n";
     }
 }
 
 void solve_linear_equation() {
     double b, c;
 
-    cout << "\n--- линейное уравнение ---\n";
-    cout << "введите значение b (не равное нулю): ";
-    while (!(cin >> b) || b == 0) {
-        cout << "ошибка - 'b' не должно быть нулём - повторите ввод: ";
+    std::cout << "\n--- линейное уравнение (b * x + c = 0) ---\n";
+
+    std::cout << "введите значение b (не равное нулю): ";
+    while (!(std::cin >> b) || b == 0) {
+        std::cout << "ошибка: b не должно быть равно нулю. повторите ввод: ";
         clear_input();
     }
 
-    cout << "введите значение c: ";
-    while (!(cin >> c)) {
-        cout << "ошибка ввода - повторите ввод: ";
+    std::cout << "введите значение c: ";
+    while (!(std::cin >> c)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
     double x = -c / b;
-    cout << "решение уравнения: x = " << x << "\n\n";
+    std::cout << "решение уравнения: x = " << x << "\n\n";
 }
 
 void solve_quadratic_equation() {
     double a, b, c;
 
-    cout << "\n--- квадратное уравнение ---\n";
+    std::cout << "\n--- квадратное уравнение (a * x^2 + b * x + c = 0) ---\n";
 
-    cout << "введите значение a: ";
-    while (!(cin >> a)) {
-        cout << "ошибка ввода - повторите ввод: ";
+    std::cout << "введите значение a: ";
+    while (!(std::cin >> a)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
-    cout << "введите значение b: ";
-    while (!(cin >> b)) {
-        cout << "ошибка ввода - повторите ввод: ";
+    std::cout << "введите значение b: ";
+    while (!(std::cin >> b)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
-    cout << "введите значение c: ";
-    while (!(cin >> c)) {
-        cout << "ошибка ввода - повторите ввод: ";
+    std::cout << "введите значение c: ";
+    while (!(std::cin >> c)) {
+        std::cout << "ошибка ввода - повторите ввод: ";
         clear_input();
     }
 
     double discriminant = b * b - 4 * a * c;
 
     if (discriminant > 0) {
-        double root1 = (-b + sqrt(discriminant)) / (2 * a);
-        double root2 = (-b - sqrt(discriminant)) / (2 * a);
-        cout << "два действительных корня: x1 = " << root1 << ", x2 = " << root2 << "\n\n";
+        double root1 = (-b + std::sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - std::sqrt(discriminant)) / (2 * a);
+        std::cout << "2 действительных корня: x1 = " << root1 << ", x2 = " << root2 << "\n\n";
     }
     else if (discriminant == 0) {
         double root = -b / (2 * a);
-        cout << "один действительный корень: x = " << root << "\n\n";
+        std::cout << "1 действительный корень: x = " << root << "\n\n";
     }
     else {
-        double real_part = -b / (2 * a);
-        double imaginary_part = sqrt(-discriminant) / (2 * a);
-        cout << "два комплексных корня: x1 = " << real_part << " + " << imaginary_part << "i, "
-            << "x2 = " << real_part << " - " << imaginary_part << "i\n\n";
+        double realPart = -b / (2 * a);
+        double imaginaryPart = std::sqrt(-discriminant) / (2 * a);
+        std::cout << "2 комплексных корня: x1 = " << realPart << " + " << imaginaryPart << "i, "
+            << "x2 = " << realPart << " - " << imaginaryPart << "i\n\n";
     }
 }
 
 void clear_input() {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
